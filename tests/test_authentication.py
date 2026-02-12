ffe8f633a772ba3ccea40685eaddda5766539014
killingkaa <killingkaa@gmail.com>
2026-02-13 01:51:00 +0300

    Написание API автотестов используя Pytest. Практика написания API автотестов

---
+++ tests/test_authentication.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code, assert_equal, assert_is_true
+from clients.users.public_users_client import get_public_users_client
+
+
+def test_login():
+    public_users_client = get_public_users_client()
+    authentication_client = get_authentication_client()
+
+    create_user_request = CreateUserRequestSchema()
+    public_users_client.create_user(create_user_request)
+
+    login_request = LoginRequestSchema(email=create_user_request.email, password=create_user_request.password)
+    login_response = authentication_client.login_api(login_request)
+    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+
+    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_login_response(login_response_data)
+    assert_equal(login_response_data.token.token_type,"bearer","token_type")
+    assert_is_true(login_response_data.token.access_token, "access_token")
+    assert_is_true(login_response_data.token.refresh_token, "refresh_token")
+
