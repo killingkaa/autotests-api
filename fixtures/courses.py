aa25bf1cc1e3c36c74c38a477fa7229e23ff9536
killingkaa <killingkaa@gmail.com>
2026-07-11 03:49:53 +0300

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- fixtures/courses.py
+++ fixtures/courses.py
@@ -7,7 +7,7 @@ from fixtures.users import UserFixture
 from fixtures.files import FileFixture
 
 
-class CoursesFixture(BaseModel):
+class CourseFixture(BaseModel):
     request: CreateCourseRequestSchema
     response: CreateCourseResponseSchema
 
@@ -22,7 +22,10 @@ def function_course(
         courses_client: CoursesClient,
         function_user: UserFixture,
         function_file: FileFixture
-):
-    request = CreateCourseRequestSchema()
+) -> CourseFixture:
+    request = CreateCourseRequestSchema(
+        preview_file_id=function_file.response.file.id,
+        created_by_user_id=function_user.response.user.id
+    )
     response = courses_client.create_course(request)
-    return CoursesFixture(request=request, response=response)
+    return CourseFixture(request=request, response=response)
